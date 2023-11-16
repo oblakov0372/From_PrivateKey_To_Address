19d9838ffe22c1d25a2c1f542233ddf7f5d37486
oblakov0372 <demaoblak1@gmail.com>
2023-11-16 12:55:45 +0200

    Added the completed project

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+import json
+from eth_account import Account
+
+def get_address(private_key):
+  account = Account.from_key(private_key)
+  return account.address
+  
+def save_to_json(data):
+    filename = f'all_data.json'
+    with open(filename, 'w') as jsonfile:
+        json.dump(data, jsonfile, indent=2)
+
+def save_to_csv(data):
+    filename = "all_data.csv"
+    fields = ["private_key", "wallet_address"]
+
+    with open(filename, mode="w", newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        
+        writer.writeheader()
+
+        writer.writerows(data)
+    
+
+def main():
+  print("-------------------------------------------")
+  print("****************Oblakov_0372***************")
+  print()
+  all_data = []
+  with open('./private_keys.txt', 'r') as file:
+        private_keys = file.read().splitlines()
+  
+  for private_key in private_keys:
+      all_data.append({"private_key"  : private_key,"wallet_address": get_address(private_key)})
+  save_to_json(all_data)
+  save_to_csv(all_data)
+
+if __name__ == '__main__':
+    main()
