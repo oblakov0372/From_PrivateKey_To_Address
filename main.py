fce818cca9009caa552e9a2340ec569b20655540
oblakov0372 <demaoblak1@gmail.com>
2023-11-16 13:26:39 +0200

    add gitignore; modified main.py with try catch

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import json
 from eth_account import Account
 
 def get_address(private_key):
-  account = Account.from_key(private_key)
-  return account.address
-  
+    try:
+        account = Account.from_key(private_key)
+        return account.address
+    except ValueError as e:
+        print(f"Error processing private key: {private_key}. Reason: {str(e)}")
+        return None
+
 def save_to_json(data):
     filename = f'all_data.json'
     with open(filename, 'w') as jsonfile:
@@ -17,24 +21,25 @@ def save_to_csv(data):
 
     with open(filename, mode="w", newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
-        
         writer.writeheader()
-
         writer.writerows(data)
-    
 
 def main():
-  print("-------------------------------------------")
-  print("****************Oblakov_0372***************")
-  print()
-  all_data = []
-  with open('./private_keys.txt', 'r') as file:
+    print("-------------------------------------------")
+    print("****************Oblakov_0372***************")
+    print()
+    all_data = []
+    
+    with open('./private_keys.txt', 'r') as file:
         private_keys = file.read().splitlines()
-  
-  for private_key in private_keys:
-      all_data.append({"private_key"  : private_key,"wallet_address": get_address(private_key)})
-  save_to_json(all_data)
-  save_to_csv(all_data)
+
+    for private_key in private_keys:
+        wallet_address = get_address(private_key)
+        if wallet_address is not None:
+            all_data.append({"private_key": private_key, "wallet_address": wallet_address})
+
+    save_to_json(all_data)
+    save_to_csv(all_data)
 
 if __name__ == '__main__':
     main()
